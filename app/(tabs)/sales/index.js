5001b975f4506086e007923a69c1a7d44d1aa20c
kevinmm001 <kevinm@teknobuilt.com>
2026-06-10 07:06:25 +0300

    fixed a label issue in the sales list

--- app/(tabs)/sales/index.js
+++ app/(tabs)/sales/index.js
@@ -85,7 +85,7 @@ export default function SalesList() {
       {sales.length ? 
         <View style={styles.statsRow}>
           <StatCard
-            label="Cash on Hand"
+            label="Cash Collected"
             value={stats?.cashCollected?.toLocaleString()}
             subText=""
           />
