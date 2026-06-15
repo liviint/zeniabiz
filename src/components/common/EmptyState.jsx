cdc7c3ce0071922b45a32b72828f12456bb67ba3
kevinmm001 <kevinm@teknobuilt.com>
2026-06-15 15:55:49 +0300

    improved the header UI

--- src/components/common/EmptyState.jsx
+++ src/components/common/EmptyState.jsx
@@ -22,6 +22,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         paddingHorizontal: 16,
+        paddingVertical: 16,
         alignItems:"center",
     },
     card: {
