884df61ad75aa82f25bcf6cdc623eb85bae1f0f1
kevinmm001 <kevinm@teknobuilt.com>
2026-07-15 09:37:33 +0300

    added the core part of monetization

--- app/(tabs)/subscriptions/index.js
+++ app/(tabs)/subscriptions/index.js
@@ -2,9 +2,11 @@ import { useRevenueCat } from "@/src/components/AppDataProvider/RevenueCatProvid
 import PremiumPlusPage from "../../../src/components/subscriptions/PremiumPlusPage";
 import PremiumPage from "../../../src/components/subscriptions/PremiumPage";
 import SubScribePage from "../../../src/components/subscriptions/SubScribePage";
+import { AnalyticsService } from "../../../src/utils/analyticsService";
 
 const Subscriptions = () => {
     const { hasPremium, hasPremiumPlus } = useRevenueCat();
+    AnalyticsService.logFirstEvent('first_subscription_page_viewed');
 
     if (hasPremiumPlus) {
         return <PremiumPlusPage />;
